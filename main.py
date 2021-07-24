2c8b5ae3ce89169db459b602ff0b6e5dfa272671
Claude SIMON <epeios-q37@users.noreply.github.com>
2021-07-24 11:09:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python3
+"""
+MIT License
+
+Copyright (c) 2021 Claude SIMON (https://q37.info/s/rmnmqd49)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os, sys
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.extend(["../../atlastk", "."])
+
+import atlastk, random
+
+DEBUG = True  # Uncomment for debug mode.
+
+LIMIT = 100
+
+SPY = 0
+PLAY = 1
+WAIT = 2
+
+class User:
+  def __init__(self):
+    self.player = 0
+
+
+def debug():
+  try:
+    return DEBUG
+  except:
+    return False
+
+
+def init():
+  global current, available, scores, turn, dice
+  current = 1 # 1 or 2
+
+  available = 1 # 0 (no more player available), 1 or 2
+
+  scores = {
+    1: 0,
+    2: 0
+  }
+
+  turn = 0  
+  dice = 0
+
+
+def get_status(player):
+  if player == 0:
+    if available == 0:
+      return SPY
+    elif available == current:
+      return PLAY
+    else:
+      return WAIT
+  elif player == current:
+    return PLAY
+  else:
+    return WAIT
+
+
+def fade(dom, element):
+  dom.remove_class(element, "fade-in")
+  dom.flush()
+  dom.add_class(element, "fade-in")
+
+
+def update_meter(dom, ab, score, turn):
+  if current + turn > LIMIT:
+    turn = LIMIT - current
+
+  fade(dom, f"TurnMeter{ab}")
+  dom.set_attribute(f"TurnMeter{ab}", "style", f"width: {turn}%;")
+
+  dom.set_attribute(f"ScoreMeter{ab}", "style", f"width: {score}%;")
+
+  dom.set_content(f"ScoreText{ab}", score)
+
+
+def disable_play_buttons(dom):
+  dom.disable_elements(["Roll", "Hold"])
+
+
+def enable_play_buttons(dom):
+  dom.enable_elements(["Roll", "Hold"])
+
+
+def get_opponent(player_ab):
+  if player_ab == 'A':
+    return 'B'
+  elif player_ab == 'B':
+    return 'A'
+  elif player_ab == 1:
+    return 2
+  else:
+    return 1
+
+
+def mark_player(dom, ab):
+  dom.remove_class(f"Marker{ab}", "hidden")
+  dom.add_class(f"Marker{get_opponent(ab)}", "hidden") 
+
+
+def display_dice(dom, value):
+  fade(dom, "dice")
+
+  if value <= 6:
+    filename = f"dice-{value}.svg" if value != 0 else "Pig.svg"
+    dom.inner("dice", open(filename).read())
+
+
+def update_meters(dom, status):
+  if status == SPY:
+    update_meter(dom, 'A', scores[1], turn if current == 1 else 0)
+    update_meter(dom, 'B', scores[2], turn if current == 2 else 0)
+  else:
+    a = current
+    me = True
+
+    if status == WAIT:
+      a = get_opponent(a)
+      me = False
+
+    b = get_opponent(a)
+
+    update_meter(dom, 'A', scores[a], turn if me else 0)
+    update_meter(dom, 'B', scores[b], 0 if me else turn)
+
+
+def update_markers(dom, status):
+  if status == WAIT:
+    mark_player(dom, 'B')
+  elif status == PLAY:
+    mark_player(dom, 'A')
+  else: # SPY
+    mark_player(dom, 'A' if current == 1 else 'B')
+
+
+def update_play_buttons(dom, status, winner):
+  if status != PLAY or winner != 0:
+    disable_play_buttons(dom)
+  else:
+    enable_play_buttons(dom)
+
+
+def display_turn(dom, element, value):
+    fade(dom, element)
+    dom.set_content(element, value)
+
+
+def update_dice(dom, winner):
+  if winner != 0 or turn != 0 or dice == 0:
+      display_dice(dom, dice)
+
+
+def update_turn(dom, winner):
+  if winner == 0:
+    display_turn(dom, "Cumul", turn if turn != -1 else 0)
+    display_turn(dom, "Total", scores[current] + (turn if turn != -1 else 0))
+
+
+def report_winner(dom, player, winner):
+  if winner == player or player == 0 and winner == 1:
+    ab = 'A'
+  else:
+    ab = 'B'
+
+  dom.set_content(f"ScoreMeter{ab}", "Winner!")
+
+
+def update_layout(dom, player):
+  status = get_status(player)
+
+  if scores[1] >= LIMIT:
+    winner = 1
+  elif scores[2] >= LIMIT:
+    winner = 2
+  else:
+    winner = 0
+
+  update_play_buttons(dom, status, winner)
+  update_dice(dom, winner)
+  update_turn(dom, winner)
+  update_meters(dom, status)
+  update_markers(dom, status)
+
+  if winner != 0:
+    report_winner(dom, player, winner)
+
+
+def display(dom, user):
+  if user.player != 0:
+    dom.enable_element("New")
+
+  if available == 0 and user.player == 0:
+    dom.set_contents({
+      "LabelA": "Player 1",
+      "LabelB": "Player 2"
+    })
+
+  update_layout(dom, user.player)
+
+
+def ac_connect(user, dom):
+  dom.inner("", open("Main.html").read())
+
+  if debug():
+    dom.remove_class("debug", "removed")
+
+  display(dom, user)
+
+
+def ac_roll(user, dom):
+  global available, current, dice, turn, scores
+
+  disable_play_buttons(dom)
+
+  if user.player == 0:
+    user.player = current
+
+    if available == 1:
+      available = 2
+    elif available == 2:
+      available = 0
+  elif user.player != current:
+    return
+
+  dice = random.randint(1, 6)
+
+  if debug():
+    debug_dice = dom.get_content("debug")
+    if debug_dice:
+      dice = int(debug_dice)
+
+  if dice == 1:
+    current = get_opponent(current)
+    turn = -1 # To force the displaying of the dice.
+  else:
+    turn += dice
+
+    if scores[current] + turn >= LIMIT:
+      scores[current] = LIMIT
+      turn = 0
+
+  atlastk.broadcast_action("Display")
+
+
+def ac_hold(user, dom):
+  global scores, turn, current
+
+  disable_play_buttons(dom)  
+
+  if user.player != current:
+    return
+
+  scores[user.player] += turn
+  current = get_opponent(current)
+  turn = 0
+  atlastk.broadcast_action("Display")
+
+
+def ac_new(dom):
+  init()
+  atlastk.broadcast_action("Display")
+
+
+def ac_display(user, dom):
+  if current == 1 and available == 1:
+    dom.inner("", open("Main.html").read())
+    if debug():
+      dom.remove_class("debug", "removed")
+    user.player = 0
+  display(dom, user)
+
+
+CALLBACKS = {
+  "": ac_connect,
+  "Roll": ac_roll,
+  "Hold": ac_hold,
+  "New": ac_new,
+  "Display": ac_display
+}
+
+init()
+
+atlastk.launch(CALLBACKS, User, open("Head.html").read())
